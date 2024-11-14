930587d1b79c02a5173a2e2e94cb9ebf08af5529
Fatih Yarinineli <fatih.yarinineli@gmail.com>
2024-11-14 10:09:19 +0100

    Alle Inhalte vom Feature branch gelöscht um von neu anzufangen :D

--- uis/start_page.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st 
-from navigate import navigate
-
-def start_page():
-    st.title("Einführung in das Knapsack-Problem")
-    st.write("Die App startet mit einer Einführung in das Knapsack-Problem, die dem Benutzer eine einfache Erläuterung der Problemstellung bietet.")
-    if st.button("Weiter"):
-        navigate("Eingabemaske")
