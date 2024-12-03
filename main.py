d8d16e7c8de34fcdf58d0a1b9c6f654c4a117b57
Fatih Yarinineli <fatih.yarinineli@gmail.com>
2024-12-03 10:08:33 +0100

    Remove unused components from main.py to streamline the codebase

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from gurobipy import Model, GRB, quicksum
 from uis.start_page import start_page
 from uis.eingabemaske_tasche import eingabemaske_tasche
-from uis.uebersichtsmaske import uebersichtsmaske
 from uis.algorithmen_auswahlmaske import algorithmen_auswahlmaske
-from uis.ladescreen import ladescreen
 from uis.ergebnismaske import ergebnismaske
 from uis.eingabemaske_gegenstände import eingabemaske_gegenstände
 
@@ -24,8 +19,6 @@ pages = {
     "Eingabemaske_Tasche": lambda: eingabemaske_tasche(pages),
     "Eingabemaske_Gegenstände": lambda: eingabemaske_gegenstände(pages),
     "Algorithmen-Auswahlmaske": lambda: algorithmen_auswahlmaske(pages),
-    "Übersichtsmaske": lambda: uebersichtsmaske(pages),
-    "Ladescreen": lambda: ladescreen(pages),
     "Ergebnismaske": lambda: ergebnismaske(pages),
 }
 
